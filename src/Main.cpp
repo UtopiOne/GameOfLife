8919df350705e60c2021e2c0d53bafbc092b94ab
UtopiOne <arseniyver@gmail.com>
2024-07-28 21:04:00 +0200

    Initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,21 @@
+﻿#include <raylib.h>
+
+constexpr int WIDTH = 1280;
+constexpr int HEIGHT = 720;
+
+int main() {
+  InitWindow(WIDTH, HEIGHT, "Game of Life");
+
+  SetTargetFPS(60);
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+
+    ClearBackground(BLACK);
+    DrawFPS(0, 0);
+
+    EndDrawing();
+  }
+
+  CloseWindow();  
+}
